fdf53a6ecd180c1b0ce6ec9f4c3104fe699d556c
gunadeau <gunadeau@hotmail.com>
2025-05-19 12:52:36 -0400

    Update horraire.ps1

--- horraire.ps1
+++ horraire.ps1
@@ -270,6 +270,8 @@ if ($matchesToday) {
         $feedBody = @{
             message = $messageEncoded
             access_token = $accessToken
+            published = $true
+            privacy = @{ value = "EVERYONE" } | ConvertTo-Json
         }
         $feedBodyJson = $feedBody | ConvertTo-Json -Depth 3 -Compress
         Write-Output "Corps de la requête pour /feed : $feedBodyJson"
